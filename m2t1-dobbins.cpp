4c303ad7c7dd914b4d8df36c578d9e997b594fa4
kdobbins1 <kdobbins910@gmail.com>
2025-02-16 22:53:34 +0000

    m2t1 done

--- m2t1-dobbins.cpp
+++ m2t1-dobbins.cpp
@@ -1,6 +1,6 @@
-// CSC 134
-// M2T1
-// Keion
+// csc 134
+// m2t1
+// keion
 // 2/16/24
 
 #include <iostream>
@@ -9,8 +9,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello" << endl;
-    cout << "world" << endl;
-    cout << endl;
+    // variables are like mailboxes
+    string name = "kd";
+    int num_apples = 10;            // int are whole numbers, like 3 but not 3.5
+    double price_each = 0.25;       // double stands for floating point -- numbers like 3.5, 1.23, or 7.193759
+
+    // think of it like an assembly line -- put words or numbers on in the order they should print out.
+    cout << "welcome to the " << name << " apple farm!" << endl;
+    cout << "we have " << num_apples << " in stock" << endl;
+    cout << "each apple costs " << price_each << endl;
+
+    // let's do math
+    double total_price = num_apples * price_each;
+
+    // finally print answer
+    cout << " " << endl;
+    cout << "for " << num_apples << " kd apples" << endl;
+    cout << "your total is: $" << total_price << endl;
+    cout << "thank you! come again!" <<endl;
+    cout << " " << endl;
+
     return 0;
 }
