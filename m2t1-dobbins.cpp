db48a3101f540ccfe2a5c26088aa1a0339e0c7b7
kdobbins1 <kdobbins910@gmail.com>
2025-02-16 22:19:49 +0000

    mt21 starter

---
+++ m2t1-dobbins.cpp
@@ -0,0 +1,16 @@
+// CSC 134
+// M2T1
+// Keion
+// 2/16/24
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello" << endl;
+    cout << "world" << endl;
+    cout << endl;
+    return 0;
+}
